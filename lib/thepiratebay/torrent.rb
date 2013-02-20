80f0117b827b2f25c3d43c5bcfa439287a82d800
Nicolas Oga <2112.oga@gmail.com>
2013-02-20 12:10:35 -0300

    Bug Fix
    Code was broke, seemingly because torrent attributes list can be different for each torrent, so I made a Dynamic version.

--- lib/thepiratebay/torrent.rb
+++ lib/thepiratebay/torrent.rb
@@ -3,33 +3,29 @@ require 'open-uri'
 
 module ThePirateBay
   class Torrent
+    PROCESS = Hash.new(->(e){ e.text.gsub(/\n|\t|\s$|^\s/, '') }).merge({
+      files: ->(e){ e.text.to_i },
+      size: ->(e){ e.text.scan(/\d+/)[-1].to_i },
+      info: ->(e){ e.at('a')[:href] },
+      spoken_languages: ->(e){ e.text.split(', ') },
+      tags: ->(e){ e.search('a').map(&:text) },
+      uploaded: ->(e){ Time.gm *e.text.scan(/\d+/).reject(&:empty?).map(&:to_i) },
+      seeders: ->(e){ e.text.to_i },
+      leechers: ->(e){ e.text.to_i },
+      comments: ->(e){ e.text.to_i }
+    })
+
     def self.find(torrent_id)
-
       doc = Nokogiri::HTML(open('http://thepiratebay.org/torrent/' + torrent_id.to_s))
-
-      contents = doc.search('#detailsframe')
-      title = contents.search('#title').text.strip
-      category = contents.search('#details .col1 dd')[0].text
-      nr_files = contents.search('#details .col1 dd')[1].text.to_i
-      size = contents.search('#details .col1 dd')[2].text
-      uploaded = contents.search('#details .col2 dd')[1].text
-      seeders = contents.search('#details .col2 dd')[3].text.to_i
-      leechers = contents.search('#details .col2 dd')[4].text.to_i
-      torrent_link = contents.search('#details .download a')[0]['href']
-      magnet_link = contents.search('#details .download a')[1]['href']
-      description = contents.search('#details .nfo pre').text
-
-      torrent = {:title => title,
-                 :category => category,
-                 :files => nr_files, :size => size,
-                 :uploaded => uploaded,
-                 :seeders => seeders,
-                 :leechers => leechers,
-                 :torrent_link => torrent_link,
-                 :magnet_link => magnet_link,
-                 :description => description}
-
-      return torrent
+      keys = doc.search('dt').map { |key| key.text.gsub(/:$/, '').gsub(/\(s\)/, 's').gsub(/\s/, '_').downcase.to_sym }
+      values = doc.search('dd')
+      attr_hash = Hash[Array.new(keys.size) { |n| [keys[n], values[n]] }].merge({ 
+        title: doc.at('#title'),
+        info_hash: doc.at('dl').children.last
+      })
+      hash = {}
+      attr_hash.each { |k,v| hash[k] = PROCESS[k].call(v) }
+      hash
     end
   end
 end
