671fbbad15592614c222cd5e4ea0fbbae8ec496a
Emanuel Andersson <manusdude@gmail.com>
2013-03-25 19:06:04 +0100

    TPB API changes

--- lib/thepiratebay/search.rb
+++ lib/thepiratebay/search.rb
@@ -16,20 +16,21 @@ module ThePirateBay
       doc.css('#searchResult tr').each do |row|
         title = row.search('.detLink').text
         next if title == ''
-        seeders = row.search('td')[2].text.to_i
-        leechers = row.search('td')[3].text.to_i
-        torrent_link = row.search('td a')[3]['href']
-        magnet_link = row.search('td a')[4]['href']
-        category = row.search('td a')[0].text
-        torrent_id = torrent_link.scan(/\d+/)[0]
 
-        torrent = {:title => title,
-                   :seeders => seeders,
-                   :leechers => leechers,
-                   :torrent_link => torrent_link,
+        seeders     = row.search('td')[2].text.to_i
+        leechers    = row.search('td')[3].text.to_i
+        magnet_link = row.search('td a')[3]['href']
+        category    = row.search('td a')[0].text
+        url         = row.search('.detLink').attribute('href').to_s
+        torrent_id  = url.split('/')[2]
+
+        torrent = {:title       => title,
+                   :seeders     => seeders,
+                   :leechers    => leechers,
                    :magnet_link => magnet_link,
-                   :category => category,
-                   :torrent_id => torrent_id}
+                   :category    => category,
+                   :torrent_id  => torrent_id,
+                   :url         => url}
 
         torrents.push(torrent)
       end
--- lib/thepiratebay/torrent.rb
+++ lib/thepiratebay/torrent.rb
@@ -7,29 +7,36 @@ module ThePirateBay
 
       doc = Nokogiri::HTML(open('http://thepiratebay.org/torrent/' + torrent_id.to_s))
 
-      contents = doc.search('#detailsframe')
-      title = contents.search('#title').text.strip
-      category = contents.search('#details .col1 dd')[0].text
-      nr_files = contents.search('#details .col1 dd')[1].text.to_i
-      size = contents.search('#details .col1 dd')[2].text
-      uploaded = contents.search('#details .col2 dd')[1].text
-      seeders = contents.search('#details .col2 dd')[3].text.to_i
-      leechers = contents.search('#details .col2 dd')[4].text.to_i
-      torrent_link = contents.search('#details .download a')[0]['href']
+      dd_cache = contents.search('#details dd').select{|dd| is_a_number?(dd.text) }
+
+      contents    = doc.search('#detailsframe')
+      title       = contents.search('#title').text.strip
+      category    = contents.search('#details .col1 dd')[0].text
+      nr_files    = dd_cache[0].text
+      size        = contents.search('#details .col1 dd')[2].text
+      uploaded    = contents.search('#details dd').select{|dd| dd.text.include?("GMT") }[0].text
+      seeders     = dd_cache[1].text
+      leechers    = dd_cache[2].text
       magnet_link = contents.search('#details .download a')[1]['href']
       description = contents.search('#details .nfo pre').text
+      url         = 'http://thepiratebay.org/torrent/' + torrent_id.to_s
 
-      torrent = {:title => title,
-                 :category => category,
-                 :files => nr_files, :size => size,
-                 :uploaded => uploaded,
-                 :seeders => seeders,
-                 :leechers => leechers,
-                 :torrent_link => torrent_link,
+      torrent = {:title       => title,
+                 :category    => category,
+                 :files       => nr_files, 
+                 :size        => size,
+                 :uploaded    => uploaded,
+                 :seeders     => seeders,
+                 :leechers    => leechers,
                  :magnet_link => magnet_link,
-                 :description => description}
+                 :description => description,
+                 :url         => url}
 
       return torrent
+    end
+
+    def self.is_a_number?(s)
+      s.to_s.match(/\A[+-]?\d+?(\.\d+)?\Z/) == nil ? false : true 
     end
   end
 end
